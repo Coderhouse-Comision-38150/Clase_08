fc3208f39db0437af2931a7756148876d8cb4df6
jpromanonet <programming@jpromano.net>
2022-12-05 21:18:53 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+
+const frase = "Hola Mundo, ¿cómo están?";
+
+app.get('/api/getFrase', (req, res) => {
+    res.send(frase)
+})
+
+app.get('/api/getLetra/:num', (req, res) => {
+    let num = parseInt(req.params.num)
+
+    if(!isNaN(num)) {
+        if(num >= 1 && num <= frase.length){
+            res.send(frase[num-1]);
+        } else {
+            res.send({error : "El parametro esta fuera de rango"})
+        }
+    } else {
+        res.send({error : "El parametro ingresado no es un numero"})
+    }
+})
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en el servidor ${error}`))
